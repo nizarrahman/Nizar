5094884109d993b25dbc1bae72eebe0a7e7d698e
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-04-12 05:20:52 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="text-center text-sm font-doodle p-3 pb-20 md:pb-3">
       <span className="font-sans">©</span>
-      2022 Anvaqta Tangguh Wisesa. All Rights Reserved.
+      2023 Nizar Rahman Fadillah. All Rights Reserved.
     </div>
   );
 };
